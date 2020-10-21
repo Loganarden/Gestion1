7de4e42e5e7857ca79f81b3bdef7a4e91e5a8c44
NAMINZO Loganarden <71003541+Loganarden@users.noreply.github.com>
2020-10-21 15:17:59 +0400

    1er

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends ('layout')
+
+    @section('css')
+
+    <link href="{{asset ('css/welcome.css')}}" rel="stylesheet" />
+
+    @endsection
+
+    @section('contenu')
+
+                <div class="title m-b-md">
+                   Bienvenue sur votre logiciel de gestion.
+                </div>
+
+                <div>
+
+
+    @endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Gestion</title>
+
+    @yield('css')
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="{{ asset('css/bardemenu.css') }}" rel="stylesheet" >
+
+  </head>
+  <body>
+
+
+            <div class="nav">
+
+                <ul>
+
+                        @if(auth()->check())
+
+                        <li style="float:right"><a style="background-color: rgba(196, 4, 4, 0.555);" href="/deconnexion" >Déconnexion</a></li>
+
+                        @else
+
+                            <li style="float:right"><a  href="/connexion">Conexion</a></li>
+
+                        @endif
+
+                </ul>
+
+            </div>
+
+        <div class="flex-center position-ref full-height">
+
+            <div class="content">
+
+                @yield('contenu')
+
+            </div>
+        </div>
+
+  </body>
+</html>
